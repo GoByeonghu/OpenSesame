06b40f850e29a9c4dcb59729ce6312ea4ae965ce
GoByeonghu <byeonghugo@gmail.com>
2022-12-05 23:15:09 +0900

    chore: Add SHA256 function definition

--- user/sha256.c
+++ user/sha256.c
@@ -1,5 +1,23 @@
 #include "sha256.h"
 
+int SHA256_Encode(char *target, char *result) {
+    unsigned char digest[SHA256_DIGEST_LENGTH];
+    char string[sizeof(target)];
+    strcpy(string, target);
+
+    SHA256((unsigned char*)&string, strlen(string), (unsigned char*)&digest);
+
+    char mdString[SHA256_DIGEST_LENGTH*2+1];
+
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+
+    strcpy(result,mdString);
+    printf("SHA256 digest: %s\n", mdString);
+
+
+    return 0;
+}
 /*
 //for test
 int main(){
